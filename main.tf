57c2ba52539323a90f7ec2e03911ad088dcbdaef
Meraj Kashi <55163548+meraj-kashi@users.noreply.github.com>
2022-06-29 01:22:01 +0200

    using vault or

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-# S3 bucket resource
-resource "aws_s3_bucket" "my_bucket" {
-  bucket = "cap-vault-circleci-demo"
-}
+# # S3 bucket resource
+# resource "aws_s3_bucket" "my_bucket" {
+#   bucket = "cap-vault-circleci-demo"
+# }
